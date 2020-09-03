1b57ed981a418ef4a356ab816bf160a0201c3451
sbabram <steve.b.abram@gmail.com>
2020-09-03 14:59:08 -0400

    added image to db

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -11,7 +11,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://fsbz7h7r7bftjd4zl47t5d363y.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-lf5bfzrdb5hdpna6xhu7rbcl5q"
+    "aws_appsync_apiKey": "da2-lf5bfzrdb5hdpna6xhu7rbcl5q",
+    "aws_user_files_s3_bucket": "notesbucket100209-dev",
+    "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
 
