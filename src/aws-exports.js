0185d0e045abfd9bf9c3dc77033f1ad5541ccc5a
sbabram <steve.b.abram@gmail.com>
2020-09-03 14:34:40 -0400

    added db

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_HeiYGTYbf",
     "aws_user_pools_web_client_id": "77v3mia054cftgle91pltvbrug",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://fsbz7h7r7bftjd4zl47t5d363y.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-lf5bfzrdb5hdpna6xhu7rbcl5q"
 };
 
 
