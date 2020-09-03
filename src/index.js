4fb17af92eec3254be1001f8448da423d421c6e6
sbabram <steve.b.abram@gmail.com>
2020-09-03 10:45:39 -0400

    added auth

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';import Amplify from 'aws-amplify';
+import config from './aws-exports';
+
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
